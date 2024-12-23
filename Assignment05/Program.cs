6f2c4e43a5a906ea17a82ec32c7a40d7a52e9c1c
LAPTOP-5AR3TGQ9\Hussien <hussienehab616@gmail.com>
2024-12-23 23:27:12 +0200

    Add project files.

---
+++ Assignment05/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Assignment05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Q1 
+
+
+
+            #endregion
+
+            #region Q2
+
+
+
+
+            #endregion
+
+
+            #region Q3 
+
+
+
+
+            #endregion
+        }
+    }
+}
